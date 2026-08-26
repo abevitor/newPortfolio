57cb3b45bfebe73f5f913c4cd155aed280988c18
abevitor <blueprates86@gmail.com>
2026-08-26 13:39:16 -0300

    feat: adicionar arquivo de traduções em português

---
+++ i18n/translations.ts
@@ -0,0 +1,36 @@
+export const translations = {
+    pt: {
+        nav: {
+            stat: 'STAT',
+            data: 'DATA',
+            map: 'MAP',
+            radio: 'RADIO',
+
+        },
+        statusView: {
+            role: 'ENGENHEIRO DE SOFTWARE',
+        },
+        dataScreen: {
+            objectives: 'OBJETIVOS',
+            completed: 'CONCLUÍDO',
+            acessTerminal: 'ACESSAR TERMINAL',
+            linkUnavailable: 'LINK INDISPONÍVEL',
+            featuredProject: 'PROJETO EM DESTAQUE',
+            visualizarPdf: 'Visualizar PDF',
+            baixarPdf: 'Baixar PDF',
+            carregandoPdf: 'Carregando PDF...',
+            erroPdf: 'Erro ao carregar o PDF',
+            anterior: 'Anterior',
+            proxima: 'Próxima',
+        },
+        perksView: {
+            issuer: 'Emissor',
+            acessarCertificado: 'Acessar Certificado',
+        },
+        radioScreen: {
+            signalFound: 'SINAL ENCONTRADO',
+            liveFeed: 'AO VIVO',
+            tuning: 'SINTONIA',
+        }
+    }
+}
