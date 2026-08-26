1993f6b15c92e7ed917ba2864b5bae15ab5a62b1
abevitor <blueprates86@gmail.com>
2026-08-26 14:20:40 -0300

    feat: implementar componente LanguageToggle para alternar idiomas

--- components/screens/LanguageToggle.tsx
+++ components/screens/LanguageToggle.tsx
@@ -1,2 +1,17 @@
 import React from 'react';
-import 
+import { useLanguage } from '@/i18n/LanguageContext';
+
+const LanguageToggle: React.FC = () => {
+    const {language, toggleLanguage} = useLanguage();
+  
+    return (
+        <button
+          onClick={toggleLanguage}
+          className="border border-pip text-pip px-2 py-1 text-xs font-mono -uppercase hover:bg-pip hover:text-black transition-colors shrink-0"
+          >
+            {language === 'pt' ? 'EN' : 'PT'}
+          </button>
+    );
+};
+
+export default LanguageToggle;
