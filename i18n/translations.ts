6542a643f6a99d209fbc2bca92c0be7134e5931c
abevitor <blueprates86@gmail.com>
2026-08-26 13:45:45 -0300

    feat: adicionar traduções em inglês para várias seções do site

--- i18n/translations.ts
+++ i18n/translations.ts
@@ -42,5 +42,50 @@ export const translations = {
             error: 'Erro, tente novamente',
             send: 'Enviar',
         },
-    }
-}
+    },
+
+    en: {
+        nav: {
+            stat: 'STAT',
+            data: 'DATA',
+            map: 'MAP',
+            radio: 'RADIO',
+
+        },
+        statusView: {
+            role: 'ENGENHEIRO DE SOFTWARE',
+        },
+        dataScreen: {
+            objectives: 'OBJETIVOS',
+            completed: 'CONCLUÍDO',
+            acessTerminal: 'ACESSAR TERMINAL',
+            linkUnavailable: 'LINK INDISPONÍVEL',
+            featuredProject: 'PROJETO EM DESTAQUE',
+            visualizarPdf: 'Visualizar PDF',
+            baixarPdf: 'Baixar PDF',
+            carregandoPdf: 'Carregando PDF...',
+            erroPdf: 'Erro ao carregar o PDF',
+            anterior: 'Anterior',
+            proxima: 'Próxima',
+        },
+        perksView: {
+            issuer: 'Emissor',
+            acessarCertificado: 'Acessar Certificado',
+        },
+        radioScreen: {
+            signalFound: 'SINAL ENCONTRADO',
+            liveFeed: 'AO VIVO',
+            tuning: 'SINTONIA',
+        },
+        contactForm: {
+            title: 'Transmitir Mensagem',
+            name: 'NOME',
+            email: 'EMAIL',
+            message: 'MENSAGEM',
+            sending: 'Enviando...',
+            sucess: 'Enviado!',
+            error: 'Erro, tente novamente',
+            send: 'Enviar',
+        },
+    },
+};
